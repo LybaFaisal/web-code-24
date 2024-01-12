f0f0b34edcb417bd9d8df3d89e750768e685efae
Lyba <faisallyba06@gmail.com>
2024-01-12 20:47:48 +0500

    JS Loops

---
+++ file9.js
@@ -0,0 +1,34 @@
+//Loops
+
+for( let i=1; i<10; i++)
+{
+    //document.write("iteration= " + i);
+    document.write(`iteration= ${i}`);
+}
+
+//example #2
+
+
+for( let x=0; x<=5 ;x++)
+{
+    document.write(`iteration= ${x}`);
+}
+
+//using while loop
+
+let i=5
+while(i<=10)
+{
+    document.write(`iteration= ${i}`);
+    i++;
+}
+
+//example 2:
+
+
+let x=5;
+while(x<9)
+{
+    document.write(`iteration= ${x}`);
+    x++;
+}
